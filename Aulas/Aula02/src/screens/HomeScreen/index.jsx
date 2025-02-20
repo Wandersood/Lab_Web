dc75d9d15aaa7bb4c83eaadec0863c44a24f6b63
Wanderson Ribeiro <mecanica.wanderson@gmail.com>
2025-02-20 10:09:45 -0300

    first commit

---
+++ Aulas/Aula02/src/screens/HomeScreen/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { Styles } from "./styles";
+
+const HomeScreen = () => {
+	return (
+		<View style={Styles.container}>
+			<Text style={Styles.text}>Home Screen</Text>
+		</View>
+	);
+};
+export default HomeScreen;
---
+++ Aulas/Aula02/src/screens/SobreScreen/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+
+export default function Sobre() {
+	return (
+		<View style={styleSobre.container}>
+			<Text style={styleSobre.texto}>Olá Mundo!!!</Text>
+		</View>
+	);
+}
