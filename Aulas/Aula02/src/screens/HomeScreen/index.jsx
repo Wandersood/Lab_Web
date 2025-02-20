3e0e13cf20a590d1db9915e870732b17f5de305b
Wanderson Ribeiro <mecanica.wanderson@gmail.com>
2025-02-20 11:01:04 -0300

    corrigir os erros

--- Aulas/Aula02/src/screens/HomeScreen/index.jsx
+++ Aulas/Aula02/src/screens/HomeScreen/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { Styles } from "./styles";
+import { Styles } from "./../HomeScreen/style";
 
 const HomeScreen = () => {
 	return (
--- Aulas/Aula02/src/screens/SobreScreen/index.jsx
+++ Aulas/Aula02/src/screens/SobreScreen/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { styles } from "./styles";
+import { styles } from "./../SobreScreen/styles";
 
 export default function Sobre() {
 	return (
